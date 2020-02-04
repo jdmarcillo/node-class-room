25d508edd05b791b33788d1cab4032caf8d37b91
Juan David Marcillo Alba <jmarcillo@qvision.com.co>
2020-02-03 23:46:38 -0500

    Installation of express framework and eslint based on airbnb configuration, configuration of prettier and refactoring of previus functions with express

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
-//Core modules
-const url = require("url");
-const http = require("http");
-const querystring = require("querystring");
-//Local modules
-const { info } = require("./modules/my-log")
-//Third party modules
+// Third party modules
+const express = require('express');
 const { countries } = require('countries-list');
+// Local modules
+const { info, error } = require('./modules/my-log');
+// Core modules
+// const url = require("url");
+// const http = require("http");
+// const querystring = require("querystring");
 
-var server = http.createServer(function(request, response){
+const app = express();
 
-    let parsed = url.parse(request.url);
-    let pathname = parsed.pathname;
-    let query = querystring.parse(parsed.query);
-    console.log(query);
-
-    if(pathname === '/'){
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('<html><body><p>HOME PAGE</p></body></html>');
-        response.end();
-    }else if(pathname === '/status'){
-        var result = info(pathname);
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(result);
-        response.end();
-    }else if(pathname === '/country'){
-        response.writeHead(200, {'Content-Type': 'text/json'});
-        response.write(JSON.stringify(countries[query.code]));
-        response.end();
-    }else{
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.write('<html><body><p>NOT FOUND</p></body></html>');
-        response.end();
-    }
+app.get('/', (request, response) => {
+  response.status(200).send('Hello world');
 });
-server.listen(4000);
-console.log("running on port 4000");
+
+app.get('/country', (request, response) => {
+  const { query } = request;
+  response.json(countries[query.code]);
+});
+
+app.get('*', (request, response) => {
+  response.status(404).send('NOT FOUND');
+});
+// var server = http.createServer(function(request, response){
+
+//     let parsed = url.parse(request.url);
+//     let pathname = parsed.pathname;
+//     let query = querystring.parse(parsed.query);
+//     console.log(query);
+
+//     if(pathname === '/'){
+//         response.writeHead(200, {'Content-Type': 'text/html'});
+//         response.write('<html><body><p>HOME PAGE</p></body></html>');
+//         response.end();
+//     }else if(pathname === '/status'){
+//         var result = info(pathname);
+//         response.writeHead(200, {'Content-Type': 'text/html'});
+//         response.write(result);
+//         response.end();
+//     }else if(pathname === '/country'){
+//         response.writeHead(200, {'Content-Type': 'text/json'});
+//         response.write(JSON.stringify(countries[query.code]));
+//         response.end();
+//     }else{
+//         response.writeHead(404, {'Content-Type': 'text/html'});
+//         response.write('<html><body><p>NOT FOUND</p></body></html>');
+//         response.end();
+//     }
+// });
+app.listen(4000, () => {
+  console.log('running on port 4000');
+});
